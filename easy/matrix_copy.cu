52099c2aae748cca408547be4ea0dd6843d63f7c
Komal <patkisaudamini@gmail.com>
2026-09-07 12:25:14 -0500

    Add matrix copy

---
+++ easy/matrix_copy.cu
@@ -0,0 +1,17 @@
+#include <cuda_runtime.h>
+
+__global__ void copy_matrix_kernel(const float* A, float* B, int total) {
+    int thread_i = blockDim.x * blockIdx.x + threadIdx.x ;
+    if(thread_i < total){
+        B[thread_i] = A[thread_i];
+    }
+}
+
+// A, B are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* A, float* B, int N) {
+    int total = N * N;
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (total + threadsPerBlock - 1) / threadsPerBlock;
+    copy_matrix_kernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, total);
+    cudaDeviceSynchronize();
+}
